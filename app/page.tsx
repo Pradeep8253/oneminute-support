fdaa16a72eba94b7ec42f7fc69b7c12909cf3ec0
pradeep yadav <pradeepyadav8253@gmail.com>
2026-01-30 09:22:30 +0530

    hero section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Hero from "@/components/landing/hero";
+import Navbar from "@/components/landing/nav";
+import React from "react";
 
 function Page() {
-    return (
-        <div>Page</div>
-    )
+  return (
+    <main className="w-full flex flex-col relative z-10">
+      <Navbar />
+      <Hero />
+    </main>
+  );
 }
 
-export default Page
+export default Page;
